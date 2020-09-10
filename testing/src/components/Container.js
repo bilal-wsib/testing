fcb2a4b2d470d32d0d1ab97677eca9bf1b04ab2d
bilal-wsib <bilal@wsib-innovation.ca>
2020-09-10 13:15:42 -0400

    container look

--- testing/src/components/Container.js
+++ testing/src/components/Container.js
@@ -1,13 +1,21 @@
 /*
-* Created by: :component
-* Date: 2020-09-10 11:59:40
-*/
+ * Created by: :component
+ * Date: 2020-09-10 11:59:40
+ */
 
 import React from "react";
-import {Buttons, Input, Item} from './container/index';
+import { Buttons, Input, Item } from "./container/index";
 
 export const Container = () => {
-
- return <div>NewFile</div>;
-
+  return (
+    <div className="app-container">
+      <div className="app-title">the TODO LIST</div>
+      <div className="form">
+        <div className="input-title">Add a Todo Item</div>
+        <div>
+          <input />
+        </div>
+      </div>
+    </div>
+  );
 };
