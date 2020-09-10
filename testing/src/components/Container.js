019736718cf1f2f0204f1f6b30762b5fd7a11c39
Pink <pink@Pinks-MacBook-Pro.local>
2020-09-10 14:08:07 -0400

    dev container push

--- testing/src/components/Container.js
+++ testing/src/components/Container.js
@@ -3,19 +3,23 @@
  * Date: 2020-09-10 11:59:40
  */
 
-import React from "react";
+import React, { useState } from "react";
 import { Buttons, Input, Item } from "./container/index";
 
 export const Container = () => {
+
+const [itemsToDo, changeitemsToDo] = useState(['hello', 'text', 'another']);
+
   return (
     <div className="app-container">
       <div className="app-title">the TODO LIST</div>
       <div className="form">
-        <div className="add-item-container">
-          <div className="input-title">Add a Todo Item</div>
-          <div>
-            <Input />
-          </div>
+        <div className="input-title">Add a Todo Item</div>
+        <div>
+          <input />
+          {/* {itemsToDo.map(item =>
+          )} */}
+
         </div>
       </div>
     </div>
