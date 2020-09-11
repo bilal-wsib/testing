81c29df14f3bea2b0c925eeecbc572f5cd68fc85
bilal-wsib <bilal@wsib-innovation.ca>
2020-09-11 10:06:29 -0400

    added new item button

--- testing/src/components/Container.js
+++ testing/src/components/Container.js
@@ -3,7 +3,7 @@
  * Date: 2020-09-10 11:59:40
  */
 
-import React, { useState, useReducer } from "react";
+import React, { useState, useReducer, useEffect } from "react";
 import { Buttons, Input, Item, MakeList } from "./container/index";
 
 export const Container = () => {
@@ -14,13 +14,26 @@ export const Container = () => {
     []
   );
   const [itemsToDo, changeitemsToDo] = useState(["hello", "text", "another"]);
+  //new todo title temp holder
+  const [todoName, setTodoName] = useState("");
+
+  useEffect(() => console.log(todoName), [todoName]);
+
+  //function sets the new value of title to the lastest array value
+  function handleClick() {
+    changeitemsToDo([...itemsToDo, todoName]);
+    setTodoName("");
+  }
 
   return (
     <div className="app-container">
       <div className="app-title">the TODO LIST</div>
       <div className="form">
         <div className="input-title">Add a Todo Item</div>
-        <Input />
+        <Input setTodoName={setTodoName} todoName={todoName} />
+        <button className="Add-Item-Button" onClick={handleClick}>
+          Add item
+        </button>
 
         <div className="" style={{ marginTop: "4vmin" }}>
           <div style={{ paddingBottom: "4vmin" }}>TODOs:</div>
